5ea3dda6479d2ef081b2764ee0ccd22db2f04ac8
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2025-06-13 17:59:05 +0200

    chore: bump version to 1.0.1

--- Formula/iterm-splitter.rb
+++ Formula/iterm-splitter.rb
@@ -3,7 +3,7 @@ class ItermSplitter < Formula
   homepage "https://github.com/your-username/its"
   url "https://github.com/your-username/its/releases/latest/download/iterm-splitter"
   sha256 "63b959852a073c7a67d14d84d26d1e72c6532667d6523b40787a14d1af54418f"
-  version "1.0.0"
+  version "1.0.1"
 
   depends_on :macos
   depends_on "iterm2"
